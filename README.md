c1dbe9a8cad1b00c7493f17745b6122f24caea78
MarqBruh <martin.07.hyatt@gmail.com>
2022-10-18 22:08:25 +1100

    add cooler bpwf :)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BombParty Word Finder
 
-Finds the most efficient words to make it easier to get extra lives in bombparty, that's about it
+Finds the most efficient words to make it easier to get extra lives in bombparty, that's about it.
+
+Please use the cooler BPWF and not the other one, it calculates the best words to do based on the letters you've already used.
---
+++ the cooler bpwf.py
@@ -0,0 +1,51 @@
+import os, copy
+
+dict = {}
+original_dict = {}
+ulc = []
+used_letters = []
+used_words = []
+suggested_word_count = 5
+
+
+
+with open('words.txt', encoding='utf8') as words:
+    for word in words:
+        current_word = ''.join(word.splitlines())
+        for letter in current_word:
+            if letter not in ulc and letter != 'x' and letter != 'z' and letter != '-':
+                ulc.append(letter)
+            dict[current_word] = len(ulc)          
+        ulc.clear()
+
+original_dict = copy.copy(dict)
+
+while True:
+    for word in dict:
+        for letter in used_letters:
+            if letter in word:
+                dict[word] -= 0.5
+    answer = input('Prompt: ')
+
+    print('Suggested words:')
+
+    counter = 0
+
+    for word in reversed(sorted(dict, key=dict.get)):
+        if answer in word and word not in used_words:
+            print(word)
+            counter += 1
+            if counter == suggested_word_count:
+                break
+
+    answer = input('What word did you play? ')
+    used_words.append(answer)
+    for letter in answer:
+        if letter not in used_letters:
+            used_letters.append(letter)
+    used_letters.sort()
+    if used_letters == ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y']:
+        used_letters.clear()
+        dict = copy.copy(original_dict)
+        print('Extra Life!')
+    print(used_letters)
