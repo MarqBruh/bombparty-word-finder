a3641c445d561e730e3df5a5942eab6049251d00
MarqBruh <martin.07.hyatt@gmail.com>
2022-10-18 18:43:58 +1100

    fix more stuff

--- bpwf.py
+++ bpwf.py
@@ -3,8 +3,8 @@ import os
 dict = {}
 ulc = []
 
-if not os.path.isfile("ulc_cache.txt"):
-    with open("words.txt") as words:
+if not os.path.isfile('ulc_cache.txt'):
+    with open('words.txt') as words:
         for word in words:
             current_word = ''.join(word.splitlines())
             for letter in current_word:
@@ -13,26 +13,26 @@ if not os.path.isfile("ulc_cache.txt"):
                 dict[current_word] = len(ulc)          
             ulc.clear()
 
-    ulc_cache = open("ulc_cache.txt", "a+")
+    ulc_cache = open('ulc_cache.txt', 'a+')
 
     for item in reversed(sorted(dict, key=dict.get)):
-      ulc_cache.write(item + " ")
+      ulc_cache.write(item + ' ')
 
 
-x = int(input("How many top words do you want to show? "))
+x = int(input('How many top words do you want to show? '))
 
 if x == 1:
-    print("The best word in your list is ")
+    print('The best word in your list is ')
 else:
-    print("The " + str(x) + " best words in your list are: ")
+    print('The ' + str(x) + ' best words in your list are: ')
 
-with open("ulc_cache.txt") as file:
+with open('ulc_cache.txt') as file:
     print(file.read().split()[:x])
 
 while True:
     dict.clear()
-    search = input("Enter bombparty prompt or string to find all words containing that string (Sorts by ULC, ascending): ")
-    with open("words.txt") as words:
+    search = input('Enter bombparty prompt or string to find all words containing that string (Sorts by ULC, ascending): ')
+    with open('words.txt') as words:
         for word in words:
             current_word = ''.join(word.splitlines())
             for letter in current_word:
